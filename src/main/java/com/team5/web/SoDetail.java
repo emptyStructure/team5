6d87a665e447753021d286ad8ab5d8d9d90fc02e
h0-shi <whdgus9655@naver.com>
2024-01-30 18:11:03 +0900

    somoim detail 생성중

--- src/main/java/com/team5/web/SoDetail.java
+++ src/main/java/com/team5/web/SoDetail.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.team5.dao.SomoimDAO;
+import com.team5.dto.SomoimDTO;
+
 @WebServlet("/soDetail")
 public class SoDetail extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -18,6 +21,11 @@ public class SoDetail extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getParameter("sno");
+
+		SomoimDAO dao = new SomoimDAO();
+		SomoimDTO detail = dao.detail(Integer.parseInt(request.getParameter("sno")));
+		request.setAttribute("detail", detail);
 		
 		RequestDispatcher rd = request.getRequestDispatcher("soDetail.jsp");
 		rd.forward(request, response);
