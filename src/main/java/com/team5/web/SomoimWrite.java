c30886e7a02d503b9cef638cc81d3c999fe2d6dc
h0-shi <whdgus9655@naver.com>
2024-02-01 16:41:35 +0900

    somoim css 변경 몇 글 작성 기능 추가

--- src/main/java/com/team5/web/SomoimWrite.java
+++ src/main/java/com/team5/web/SomoimWrite.java
@@ -1,8 +1,8 @@
 package com.team5.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,43 +10,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.team5.dao.SomoimDAO;
-import com.team5.dto.JoinSomoimDTO;
-import com.team5.util.Util;
 
-@WebServlet("/joinsomoim")
-public class Joinsomoim extends HttpServlet {
+@WebServlet("/somoimWrite")
+public class SomoimWrite extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public Joinsomoim() {
+    public SomoimWrite() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	RequestDispatcher rd = request.getRequestDispatcher("./somoimWrite.jsp");
+	rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		String category = request.getParameter("category");
 		
-		String sno = request.getParameter("sno");
-		String mno = request.getParameter("mno");
-		String name = request.getParameter("name");
-		String ph = request.getParameter("ph");
-		String msg = request.getParameter("msg");
-
-		JoinSomoimDTO dto = new JoinSomoimDTO();
 		SomoimDAO dao = new SomoimDAO();
-		
-		dto.setSno(Util.str2Int2(sno));
-		dto.setMno(Util.str2Int2(mno));
-		dto.setName(name);
-		dto.setPh(ph);
-		dto.setMessage(msg);
-		
-		int result = dao.join(dto);
-		
-		PrintWriter pw = response.getWriter();
-		pw.print(result);
+		int result = dao.write(title, content, category);
+		if(result == 1) {
+			System.out.println("게시글 작성 성공");
+			response.sendRedirect("./somoim");
+		} else {
+			System.out.println("게시글 작성 실패");
+			response.sendRedirect("./error.jsp");
+		}
 		
 	}
 
