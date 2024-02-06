2064c5ace49ef57df1b047319b145be8886fdc45
sweetpotatotest <kgb4232@daum.net>
2024-02-06 14:50:12 +0900

    로그인 오류 수정했습니다. 패치해주세여

--- src/main/java/com/team5/web/Login.java
+++ src/main/java/com/team5/web/Login.java
@@ -43,25 +43,25 @@ public class Login extends HttpServlet {
 			dto = dao.login(dto);
 			
 			if (dto.getCount() == 1) {
-				//로그인 성공, index페이지 이동, 로그인 세션 저장
-				
-				HttpSession session = request.getSession();
-				session.setAttribute("mname", dto.getMname());//mname이라는 이름으로 세션만들기
-				session.setAttribute("mid", dto.getMid());//mid라는 이름으로 세션 만듬
-				
-				// Referer 헤더를 확인하여 이전 페이지로 Redirect
-	            String referer = request.getHeader("Referer");
-	            if (referer != null && !referer.contains("LoginServlet")) {
-	                response.sendRedirect(referer);
-	            } else {
-	                // Referer가 없거나 로그인 페이지에서 왔다면 기본적으로 index 페이지로 Redirect
-	            	//response이기 때문에 request값을 공유 하지 않음
-	                response.sendRedirect("./index");
-	            }
+			    // 로그인 성공, index 페이지 이동, 로그인 세션 저장
+			    HttpSession session = request.getSession();
+			    session.setAttribute("mname", dto.getMname()); // mname이라는 이름으로 세션 만들기
+			    session.setAttribute("mid", dto.getMid()); // mid라는 이름으로 세션 만듬
+
+			    // Referer 헤더를 확인하여 이전 페이지로 Redirect
+			    String referer = request.getHeader("Referer");
+			    if (referer != null && !referer.contains("/login")) {
+			        response.sendRedirect(referer);
+			    } else {
+			        // Referer가 없거나 로그인 페이지에서 왔다면 기본적으로 index 페이지로 Redirect
+			        response.sendRedirect("./index");
+			    }
 			} else {
-				//에러페이지? 파라미터값 = 4567
-				response.sendRedirect("./login?error=4567");
+			    // 로그인 실패
+			    // 에러페이지로 Redirect
+			    response.sendRedirect("./login?error=4567");
 			}
+
 			
 		}
 
